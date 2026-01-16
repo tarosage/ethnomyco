ba933c1decf6d457f50741324fca31c0f5272711
Taro <coleol@oregonstate.edu>
2026-01-16 10:48:44 -0800

    idk lol

--- - js/main.js
+++ - js/main.js
@@ -1,3 +1,3 @@
 /* JS Main by Taro Sage, 2026 */
 var myDiv = document.getElementById("example")
-myDiv.innerHTML = "Here's a cool and culturally significant mushroom."
+myDiv.innerHTML = "Amanita Muscaria"
