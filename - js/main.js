677037aac230a3b6806ba9c0e9d62d3f01ac4581
Taro <coleol@oregonstate.edu>
2026-01-28 10:40:34 -0800

    adding comments

--- - js/main.js
+++ - js/main.js
@@ -1,3 +1,6 @@
 // JS Main by Taro Sage, 2026 //
 var myDiv = document.getElementById("example");
 myDiv.innerHTML = "The Renowned Amanita Muscaria"
+
+<h1 style="background-color:DarkOliveGreen; font-size:25;">
+<p style="background-color:Tomato; font-size:16;">
